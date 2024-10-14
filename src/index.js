9e0ac4422f2f0d0ae7555dff3a728df768c8ca43
cristian <cristian.aiki1@gmail.com>
2024-10-14 20:19:44 -0300

    realizando cambios railway 3

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, 'views')); // Ajusta la ruta de las vistas
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
+//Vistas
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({extended:true}))
